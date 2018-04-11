334473b892709d98d94636c8be0d8406dad5e1e8
Coni <conicaveri@gmail.com>
2018-04-11 16:29:29 -0300

    Creando una branch porque si

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers
+{
+    [Route("/api/cities")]
+    public class CitiesController:Controller
+    {
+        [HttpGet("{id}")]
+        public JsonResult GetCities()
+        {
+           return new JsonResult()
+            
+        }
+
+
+    }
+}
